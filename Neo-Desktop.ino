587910f50c6f6d10a20230c361114cfb575a6bac
Curtis Malainey <curtis@malainey.com>
2015-09-25 22:26:00 -0600

    Complete Rework of client and server
    Added multi backdrop support, now runs on windows 10, fading algorithm needs some work still

--- Neo-Desktop.ino
+++ Neo-Desktop.ino
@@ -1,21 +1,90 @@
 #include "Adafruit_NeoPixel.h"
 
-#define COLORPIN 6
+#define COLORPIN  6
 #define NUMPIXELS 60
+#define DISPLAYS  3
 
-int current_color[] = {0, 0, 0};
-int target_color[] = {0, 0, 0};
+int current_color[DISPLAYS][3];
+int target_color[DISPLAYS][3];
+int display_pixels[DISPLAYS];
 char message[60];
 uint8_t str_postion = 0;
 uint16_t red;
 uint16_t green;
 uint16_t blue;
+uint16_t display;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, COLORPIN, NEO_GRB + NEO_KHZ800);
+
+void setup_displays()
+{
+  int sections = NUMPIXELS/(DISPLAYS - 1);
+  display_pixels[0] = 0;
+  for (int i = 1; i < DISPLAYS; i++)
+  {
+    display_pixels[i] = display_pixels[i - 1] + sections;
+  }
+  display_pixels[DISPLAYS - 1] = NUMPIXELS - 1;
+}
+
+bool pixel_change(int d)
+{
+  return (current_color[d][0] != target_color[d][0] || current_color[d][1] != target_color[d][1] || current_color[d][2] != target_color[d][2]);
+}
+
+int get_display(int p)
+{
+  for (int i = 0; i < DISPLAYS; i++)
+  {
+    if (p == display_pixels[i])
+      return i;
+  }
+  return -1;
+}
+
+void update_pixel(int d)
+{
+  for (int i; i < 3; i++)
+  {
+    if (target_color[d][i] > current_color[d][i])
+    {
+      current_color[d][i]++;
+    } 
+    else if (target_color[d][i] < current_color[d][i])
+    {
+      current_color[d][i]--;
+    }
+  }
+}
+
+bool is_pixel(int p)
+{
+  for (int i = 0; i < DISPLAYS; i++)
+  {
+    if (p == display_pixels[i])
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+int get_leading_pixel(int p)
+{
+  for (int i = 0; i < DISPLAYS; i++)
+  {
+    if (p < display_pixels[i])
+    {
+      return i;
+    }
+  }
+}
 
 void setup() {
     Serial.begin(9600);
     pixels.begin();
+    delay(5000);
+    setup_displays();
 }
 
 void loop() {
@@ -30,11 +99,11 @@ void loop() {
       {
         Serial.print("GREEN!\n");
       }
-      else if (sscanf(message, "(%u, %u, %u)\n", &red, &green, &blue) == 3)
+      else if (sscanf(message, "(%u, %u, %u, %u)\n", &red, &green, &blue, &display) == 4)
       {
-        target_color[0] = (uint8_t)red;
-        target_color[1] = (uint8_t)green;
-        target_color[2] = (uint8_t)blue;
+        target_color[display][0] = (uint8_t)red;
+        target_color[display][1] = (uint8_t)green;
+        target_color[display][2] = (uint8_t)blue;
       }
       else
       {
@@ -47,23 +116,54 @@ void loop() {
     }
   }
   delay(10);
-  if (current_color[0] != target_color[0] || current_color[1] != target_color[1] || current_color[2] != target_color[2])
+  if (DISPLAYS == 1)
   {
-    for (int i; i < 3; i++)
+    if (pixel_change(0))
     {
-      if (target_color[i] > current_color[i])
+      update_pixel(0);
+      for (int i; i < NUMPIXELS; i++)
       {
-        current_color[i]++;
-      } 
-      else if (target_color[i] < current_color[i])
+        pixels.setPixelColor(i, pixels.Color(current_color[0][0], current_color[0][1], current_color[0][2]));
+      }
+      pixels.show();
+    }
+  }
+  else 
+  {
+    bool change = false;
+    for (int d = 0; d < DISPLAYS; d++)
+    {
+      if (pixel_change(d))
       {
-        current_color[i]--;
+        update_pixel(d);
+        change = true;
       }
     }
-    for (int i; i < NUMPIXELS; i++)
+    if (change)
     {
-      pixels.setPixelColor(i, pixels.Color(current_color[0], current_color[1], current_color[2]));
+      for (int p = 0; p < NUMPIXELS; p++)
+      {
+        if (is_pixel(p))
+        {
+          int d = get_display(p);
+          pixels.setPixelColor(p, pixels.Color(current_color[d][0], current_color[d][1], current_color[d][2]));
+        }
+        else
+        {
+          int d = get_leading_pixel(p);
+          double leading;
+          double trailing;
+          double l_factor = 1 - ((double)display_pixels[d] - (double)p)/(double)NUMPIXELS;
+          double t_factor = 1 - ((double)p - (double)display_pixels[d-1])/(double)NUMPIXELS;
+          int colors[3];
+          for (int c = 0; c < 3; c++)
+          {
+            colors[c] = (int)(l_factor*(double)current_color[d][c] + t_factor*(double)current_color[d - 1][c]);
+          }
+          pixels.setPixelColor(p, pixels.Color(colors[0], colors[1], colors[2]));
+        }
+      }
+      pixels.show();
     }
-    pixels.show();
   }
 }
--- color.py
+++ color.py
@@ -1,15 +1,66 @@
-import subprocess
+from os import listdir
+from os.path import isfile, join
 from PIL import Image
 import scipy
 import scipy.misc
 import scipy.cluster
-import re
 import time
 import serial
 import sys
 import glob
 import io
 import binascii
+import os
+import string
+
+system_path = os.getenv('APPDATA') + "/Microsoft/Windows/Themes/"
+displayMap = {0:0, 1:1, 2:2}
+
+class background():
+    path = ""
+    displayNumber = -1
+    mtime = 0
+
+    def __init__(self, path):
+        self.displayNumber = int(path.split("_")[1])
+        self.path = system_path + path
+
+    def changed(self):
+        try:
+            t = os.stat(self.path)
+            if t != self.mtime:
+                self.mtime = t
+                return True
+        except IOError:
+            pass
+        return False
+
+    def getDominant(self):
+        NUM_CLUSTERS = 5
+
+        print 'reading image', self.path
+        try:
+            im = Image.open(self.path)
+        except IOError:
+            self.mtime = None
+            return
+        
+        im = im.resize((300, 300))      # optional, to reduce time
+        ar = scipy.misc.fromimage(im)
+        shape = ar.shape
+        ar = ar.reshape(scipy.product(shape[:2]), shape[2])
+        print 'finding clusters'
+        codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+
+        vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+        counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
+
+        index_max = scipy.argmax(counts)                    # find most frequent
+        peak = codes[index_max]
+        print 'most frequent is %s' % peak
+        print 'on display ' + str(self.displayNumber)
+        return peak
+
 
 def serial_ports():
     """ Lists serial port names
@@ -38,52 +89,36 @@ def serial_ports():
             pass
     return result
 
-current_path = ""
+onlyfiles = [ f for f in listdir(system_path) if isfile(join(system_path,f)) ]
+print onlyfiles
+backgrounds = []
+for f in onlyfiles:
+    if string.find(f, "_") != -1:
+        backgrounds.append(background(f))
+
 serials = serial_ports()
 print "Finding Serial Port"
 serial_arduino = None
 for ser in serials:
-	print "trying: " + ser
-	s = serial.Serial(ser)
-	s.write("T\n")
-	print "sending test command"
-	time.sleep(0.5)
-	print "reading response"
-	test = s.readline()
-	if test == "GREEN!\n":
-		serial_arduino = s
+    print "trying: " + ser
+    s = serial.Serial(ser)
+    s.write("T\n")
+    print "sending test command"
+    time.sleep(0.5)
+    print "reading response"
+    test = s.readline()
+    if test == "GREEN!\n":
+        serial_arduino = s
 
 if serial_arduino == None:
-	print "Unable to find serial port"
-	exit()
+    print "Unable to find serial port"
+    exit()
 
 print "entering main loop"
 while True:
-	time.sleep(0.1)
-	p = subprocess.Popen(["REG", "QUERY", "HKCU\Software\Microsoft\Internet Explorer\Desktop\General", "/v", "WallpaperSource"], stdout=subprocess.PIPE, shell=True)
-	(output, err) = p.communicate()
-	p_status = p.wait()
-	output = re.search("""([A-Z]:\\\\)([\\w\\d\\.\\s-]+\\\\)+([\\w\\d\\.\\s-]+\\.\\w+)""", output)
-	path = output.group(0)
-
-	if current_path != path:
-		current_path = path
-		NUM_CLUSTERS = 5
-
-		print 'reading image', path
-		im = Image.open(path)
-		im = im.resize((300, 300))      # optional, to reduce time
-		ar = scipy.misc.fromimage(im)
-		shape = ar.shape
-		ar = ar.reshape(scipy.product(shape[:2]), shape[2])
-		print 'finding clusters'
-		codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-
-		vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
-		counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
-
-		index_max = scipy.argmax(counts)                    # find most frequent
-		peak = codes[index_max]
-		print 'most frequent is %s' % peak
-		
-		serial_arduino.write(tuple(peak).__str__() + "\n")
+    time.sleep(0.1)
+    for b in backgrounds:
+        if b.changed() and b.mtime is not None:
+            msg = str(tuple(b.getDominant()))[0:-1] + ", " + str(displayMap[b.displayNumber]) + ")\n"
+            print "Sending: " + msg
+            serial_arduino.write(msg)
